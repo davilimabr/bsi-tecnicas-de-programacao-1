cc554525cb6962d841b51071d6080469164b9a0f
Davi Lima <davilimamoura.360@gmail.com>
2022-06-02 15:17:07 -0300

    tentando fazer a sequencia.cpp

--- Trabalhos/Trabalho 1/sequencia.cpp
+++ Trabalhos/Trabalho 1/sequencia.cpp
@@ -1,20 +1,16 @@
 #include<stdio.h>
-#include<stdio.h>
 #include <stdlib.h>
 
 int main(void){
-
-    int tamanho_S;
-    float *conjunto_S;
+    int tamanho_S, *conjunto_S;
+    int i, j, k, count;
 
     do{
-         puts("Insira o tamanho do conjunto S de n�meros:");
+        puts("Insira o tamanho do conjunto S de n�meros:");
         scanf("%d", &tamanho_S);
     }while(tamanho_S < 3);
-   
-    conjunto_S = (float *) malloc(tamanho_S * sizeof(float));
-
-    int i;
+    
+    conjunto_S = (int *) malloc(tamanho_S * sizeof(int));
     for(i = 0; i < tamanho_S; i++){
         puts("digite o n�mero:");
         scanf("%f", &conjunto_S[i]);
@@ -28,7 +24,15 @@ int main(void){
         scanf("%d", &tamanho_T);
     }while(tamanho_T < 2);
    
-    for(i = 0; i < tamanho_S; i++){
-       
+
+   int qtd_T = 0;
+    for(i = 1; i < tamanho_S; i++){
+       for(j = i; j < i + tamanho_T; j++){
+           
+           for(k = j; k < j + tamanho_T; k++){
+               printf("%d\n", conjunto_S[k]);
+       }
+       }
+       printf("\n\n");
     }
 }
