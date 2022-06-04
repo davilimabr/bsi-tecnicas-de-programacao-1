6a0597046cffa4a409fd95adb71e3a3090dd2792
davi <davilimamoura.360@gmail.com>
2022-06-04 12:45:15 -0300

    ajustes finais sequencia.cpp

--- Trabalhos/Trabalho 1/sequencia.cpp
+++ Trabalhos/Trabalho 1/sequencia.cpp
@@ -42,23 +42,27 @@ int main(void){
     printf("Quantidade de numeros inteiros no sub-conjunto t (t): %d\n", tamanho_T);
 
     //separa todos os possiveis sub conjuntos t dentro de s
-    int sub_conjunto_T, sub_conjuntos_T[tamanho_S], multiplicador;  
+    int sub_conjunto_T, sub_conjuntos_T[tamanho_S];
+    float multiplicador;  
 
     for(i = 0; i < tamanho_S; i++)
         if(i + tamanho_T <= tamanho_S){
-            multiplicador = ceil(pow(10, tamanho_T)) / 10; // Observação no final do código
+            multiplicador = pow(10, tamanho_T) / 10; 
             sub_conjunto_T = 0;
 
-            for(j = i; j < i + tamanho_T; j++){ 
+            for(j = i; j < i + tamanho_T; j++){
+                if(j < i + tamanho_T - 1)
+                    if(conjunto_S[j] > 9 && conjunto_S[j+1] > 9)
+                        conjunto_S[j] *= floor(log10(conjunto_S[j])) * 10;
+
                 sub_conjunto_T += multiplicador * conjunto_S[j];
                 multiplicador /= 10;
             }
             sub_conjuntos_T[i] = sub_conjunto_T;
         }
 
-    //lista os conjuntos t e a quantidade de repetições
     bool ja_existe, nao_existe = true;
-    int divisor, count = 0;
+    int count = 0, divisor;
 
     for(i = 0; i < tamanho_S; i++){
         count = 0;
@@ -67,29 +71,29 @@ int main(void){
                     count++;
         }
 
-        if(i != 0)
-            for(j = i - 1; j > 0; j--)
+        ja_existe = false;
+        if(i > 0)
+            for(j = i - 1; j >= 0; j--)
                     if(sub_conjuntos_T[i] == sub_conjuntos_T[j])
                         ja_existe = true;
 
         if(count > 1 && !ja_existe){
-            divisor = floor(pow(10, tamanho_T)) / 10;
+            nao_existe = false;
             sub_conjunto_T = sub_conjuntos_T[i];
-
-            while(divisor >= 1){
-                printf("%d ", sub_conjunto_T / divisor);
-                sub_conjunto_T %= divisor;
-                divisor /= 10;
+            
+            if(sub_conjunto_T > 999)
+                printf("%d - %dx\n", sub_conjunto_T, count);
+            else{
+                 divisor = floor(pow(10, tamanho_T)) / 10;
+                while(divisor >= 1){
+                    printf("%d ", sub_conjunto_T / divisor);
+                    sub_conjunto_T %= divisor;
+                    divisor /= 10;
+                }
+                printf("- %dx\n", count);
             }
-            printf("- %dx\n", count);
-                nao_existe = false;
         }
     }
     if(nao_existe)
         puts("Nao existe");
 }
-
-//Eu usei a função ceil para arredondar para cima o valor devolvido, pois a função pow estava com problemas.
-//Estava sendo devolvido um valor um pouco menor que o resultado deveria ser, acho que o problema está com o tipo
-//de parâmetro que a função espera (double) e o que eu estava mandando (int). Pesquisei e vi algumas pessoas com problemas de inconsistencia
-// com essa função também, além de outras da lib math.h. De qualquer forma, o código funciona sempre utilizando o ceil para arredondar.
