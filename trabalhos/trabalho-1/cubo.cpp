873ce1a284b2aafa07848f15df6db87d55d5cd99
davi <davilimamoura.360@gmail.com>
2022-08-16 08:30:34 -0300

    correção trabalho 1

--- trabalhos/trabalho-1/cubo.cpp
+++ trabalhos/trabalho-1/cubo.cpp
@@ -3,8 +3,8 @@
 #include<math.h>
 
 int main(void){
-    int base, potencia;
-    int i, j, count; //contadores
+    long long potencia;
+    int base;
 
     do{
         puts("digite uma numero para ser a base da potecia de 3");
@@ -18,36 +18,31 @@ int main(void){
         return 0;
     }
     
-    potencia = pow(base, 3);
-    int impares[potencia / 2];
-
-    //separa todos os impares entre 0 e base^3
-    count = 0;
-    for(i = 1; i <= potencia; i++)
-        if(i % 2 != 0){
-            impares[count] = i;
-            count++;
-        }   
-        
+    potencia = ceil(pow(base, 3));
 
     printf("%d ao cubo = ", base);
 
-    //calcula os resultados possíveis e printa
-    int soma;
-    int tamanho = potencia / 2;
-    for(i = 0; i < tamanho; i++){
+    long long soma;
+    for(long long i = 1; i < potencia; i++){
         soma = 0;
 
-        if(i + base <= tamanho)
-            for(j = i; j < i + base; j++)
-                soma += impares[j];
-
+        int qtd_impares = 0;
+        for(long long j = i; qtd_impares < base; j++)
+            if(j % 2 != 0){
+                soma += j;
+                qtd_impares++;
+            }
+        
         if(soma == potencia){
-            for(j = i; j < i + base; j++){
-                printf("%d", impares[j]);
-                
-                if(j < i + base - 1)
-                    printf(" + ");
+            qtd_impares = 0;
+            for(long long j = i; qtd_impares < base; j++){
+                if(j % 2 != 0){
+                    printf("%lld", j);
+                    qtd_impares++;
+                    
+                    if(qtd_impares < base)
+                        printf(" + ");
+                }
             }
             break;
         }
